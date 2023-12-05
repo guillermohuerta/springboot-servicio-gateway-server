7740b7ae18ca8ec8f3fbbbd4bca9a9cecd164071
Guillermo Huerta <guillermohuertarondan@gmail.com>
2023-12-04 20:14:40 -0500

    38. Configurando y probando nuestro Gateway Filter Factory personalizado

--- src/main/java/com/formacionbdi/springboot/app/gateway/filters/factory/EjemploGatewayFilterFactory.java
+++ src/main/java/com/formacionbdi/springboot/app/gateway/filters/factory/EjemploGatewayFilterFactory.java
@@ -1,10 +1,13 @@
 package com.formacionbdi.springboot.app.gateway.filters.factory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,17 +15,18 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
-public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<EjemploGatewayFilterFactory.ConfiguracionGuillermo>{
+public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<EjemploGatewayFilterFactory.Configuracion>{
 
 	private final Logger logger = LoggerFactory.getLogger(EjemploGatewayFilterFactory.class);
 
 	public EjemploGatewayFilterFactory() {
-		super(ConfiguracionGuillermo.class);
+		super(Configuracion.class);
 	}
 
 	@Override
-	public GatewayFilter apply(ConfiguracionGuillermo config) {
+	public GatewayFilter apply(Configuracion config) {
 		return (exchange, chain) -> {
+			
 			logger.info("Ejecutanto PRE gateway filter factory: " + config.mensaje);
 			
 			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
@@ -35,8 +39,19 @@ public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<Ej
 			}));
 		};
 	}
+	
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "EjemploName";
+	}
 
-	public class ConfiguracionGuillermo {
+	@Override
+	public List<String> shortcutFieldOrder() {
+		return Arrays.asList("mensaje", "cookieNombre", "cookieValor");
+	}
+	
+	public static class Configuracion {
 		private String mensaje;
 		private String cookieValor;
 		private String cookieNombre;
